11b9b9c5000b41bc598de24a04d261d2b22765af
hayoung <97607752+hangnik@users.noreply.github.com>
2022-12-13 19:09:52 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+let c = parseInt(input[2]);
+let d = parseInt(input[3]);
+let e = parseInt(input[4]);
+let f = parseInt(input[5]);
+
+console.log(`${1-a} ${1-b} ${2-c} ${2-d} ${2-e} ${8-f}`);
