9ea7cc7760ba20b5339b72079aeb7099cf779819
hayoung <97607752+hangnik@users.noreply.github.com>
2022-12-17 00:37:42 +0900

    [Bronze III] Title: 알람 시계, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+let hour = parseInt(input[0]);
+let minute = parseInt(input[1]);
+
+minute -= 45;
+
+if(minute < 0){
+    minute += 60;
+    hour--;
+    if(hour === -1){
+        hour = 23;
+    }
+}
+
+console.log(`${hour} ${minute}`);
