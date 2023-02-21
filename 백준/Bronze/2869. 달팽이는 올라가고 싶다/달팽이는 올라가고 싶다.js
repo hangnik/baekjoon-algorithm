74b75ef4e1d37cba675dd4f8be9ce961a28b4fd1
hangnik <97607752+hangnik@users.noreply.github.com>
2023-02-21 15:44:22 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 164 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -0,0 +1,6 @@
+const input = require("fs").readFileSync("dev/stdin").toString().split(" ");
+const A = Number(input[0]);
+const B = Number(input[1]);
+const V = Number(input[2]);
+
+console.log(Math.ceil((V - B) / (A - B)));
