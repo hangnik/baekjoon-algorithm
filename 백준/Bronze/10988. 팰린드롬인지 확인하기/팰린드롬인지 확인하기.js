25e15e5dc7f8f23f765ff49d8434239121968055
hangnik <97607752+hangnik@users.noreply.github.com>
2023-07-16 20:15:56 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -1,9 +1,6 @@
 const input = require("fs").readFileSync("dev/stdin").toString().trim();
-const lng = input.length / 2;
-const left = input.slice(0, lng);
-const right = input.slice(Number.isInteger(lng) ? lng : parseInt(lng) + 1);
 
-if (left === right.split("").reverse().join("")) {
+if (input === [...input].reverse().join("")) {
   console.log(1);
 } else {
   console.log(0);
