4d83ef6cbc26319de2b9dbd35beb6abc53655009
hangnik <97607752+hangnik@users.noreply.github.com>
2023-08-26 23:04:26 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,19 @@
+function solution(numbers) {
+  const arr = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  arr.forEach((v, i) => {
+    numbers = numbers.replaceAll(v, i);
+  });
+  return parseInt(numbers);
+}
